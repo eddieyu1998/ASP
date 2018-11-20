58d0eaa68c9a3c6fdb44fb88d24fdb61b1db6c1a
aj9730 <45138119+aj9730@users.noreply.github.com>
2018-11-20 08:53:43 +0800

    Add files via upload
    Displays total weight of order/ Checks whether weight requirement is satisfied. Allow customer to reset order

---
+++ urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = 'ASP'
+urlpatterns = [
+	path('', views.DefaultView, name='DefaultView'),
+	path('browse', views.browse.as_view(), name='browse'),
+	#path('browse/<int:user>', views.browse.as_view(), name='browse'),
+	path('addItem', views.addItem, name='addItem'),
+	path('checkout', views.checkout, name='checkout'),
+	path('placeOrder', views.placeOrder, name='placeOrder'),
+	path('resetOrder', views.resetOrder, name='resetOrder'),
+	path('dispatcherView', views.dispatcherView, name='dispatcherView'),
+	path('getDispatcherAction', views.getDispatcherAction, name="getDispatcherAction"),
+	path('warehouseView', views.warehouseView, name='warehouseView'),
+	path('process', views.process, name='process'),
+
+]
